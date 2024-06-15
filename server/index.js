e0bd9e7d8ed50a26d8daab017bcd14d704d3d73d
SAITEJA GOLI <sai@SAITEJAs-MacBook-Pro.local>
2024-06-15 07:52:30 +0530

    Completed

--- server/index.js
+++ server/index.js
@@ -6,19 +6,8 @@ require('dotenv').config()
 
 const app = express();
 
-const allowedOrigins = ['http://localhost:5173'];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 
